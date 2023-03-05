4fd49c46ef703ab688f67d8e69789aab71b39c8d
turalagakisiyev <agakisiyevtural6@gmail.com>
2023-03-04 23:54:07 -0800

    tural4

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import './Navbar.css'
 
 import {
-  BrowserRouter,
-  Routes,
-  Route,
   Link,
 } from "react-router-dom";
 
@@ -12,19 +9,18 @@ const Navbar = () => {
   return (
     <div id='navbar'>
       <div className="navbar-logo">
-        <Link to={"/"}><i class="fa-solid fa-piggy-bank" style={{color:"white",fontSize:"30px"}}>   CRYPTOCELL</i></Link>
+        <Link to={"/"}><i className="fa-solid fa-piggy-bank" style={{ color: "rgb(234, 152, 2)", fontSize: "20px" }}>   CRYPTOCELL</i></Link>
       </div>
       <div className="navbar-menu">
         <ul>
-          <Link to={"buycrypto"}><li ><i class="fa-solid fa-cart-shopping" style={{fontSize:"15px"}}>   Buy Crypto</i></li></Link>
-          <Link to={"charity"}><li><i class="fa-solid fa-handshake-angle" style={{fontSize:"15px"}}>  Charity</i></li></Link>
-          <Link to={"azercell"}><li><i class="fa-solid fa-tower-cell" style={{fontSize:"15px"}}>   Azercell</i></li></Link>
+          <Link to={"buycrypto"}><li ><i className="fa-solid fa-cart-shopping" style={{ fontSize: "12px" }}>   Buy Crypto</i></li></Link>
+          <Link to={"charity"}><li><i className="fa-solid fa-handshake-angle" style={{ fontSize: "12px" }}>  Charity</i></li></Link>
+          <Link to={"azercell"}><li><i className="fa-solid fa-tower-cell" style={{ fontSize: "12px" }}>   Azercell</i></li></Link>
         </ul>
       </div>
       <div className="navbar-login">
         <button>
-        <i class="fa-solid fa-user"> USER</i>
-         
+          <i style={{fontSize:"12px"}} className="fa-solid fa-user"> USER</i>
         </button>
       </div>
     </div>
--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './MainPage.css'
 
 
 export default function MainPage() {
   return (
     <div id='MainPage'>
-      <h1>Buy, trade, and donate cryptocurrencies on CryptoCell</h1>
+      <div style={{ display: "flex" }} className="upside"><h1>Buy, trade, and donate cryptocurrencies on CryptoCell</h1><Link to={"buycrypto"}><button className="MainPage-start"> Get Started </button></Link></div>
       <div className="MainPage-crypto-block">
-        <div><i class="fa-brands fa-bitcoin"></i>Bitcoin BTC <br /><span style={{color:"red"}}>$22,329</span> </div>
-        <div><i class="fa-brands fa-ethereum"></i>Ethereum ETH <br /> $1.586</div>
-        <div><i class="fa-solid fa-t"></i>Tether USDT <br /> $1.00</div>
-        <div><i class="fa-solid fa-earth-americas"></i>Binance Coin BNB <br />$289.10</div>
+        <div style={{ color: "orange" }}><i className="fa-brands fa-bitcoin"></i>Bitcoin BTC <br /> $22,329</div>
+        <div style={{ color: "blue" }}><i className="fa-brands fa-ethereum"></i>Ethereum ETH <br /> $1.586</div>
+        <div style={{ color: "green" }}><i className="fa-solid fa-t"></i>Tether USDT <br /> $1.00</div>
+        <div style={{ color: " rgb(251, 198, 66)" }}><i className="fa-solid fa-earth-americas"></i>Binance Coin BNB <br />$289.10</div>
       </div>
     </div>
   )
