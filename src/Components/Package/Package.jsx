4c3b1e9e92d5835cf260cf32044be62ebaf39e94
Alish Hajiyev <alish.haciyev@mail.ru>
2023-03-05 12:22:12 +0400

    Alish2

--- src/Components/Package/Package.jsx
+++ src/Components/Package/Package.jsx
@@ -6,7 +6,25 @@ export default function Package() {
       <div className="package-background">
         <h3 className="megabytes">500MB</h3>
       </div>
-      <div className="package-info"></div>
+      <div className="package-info">
+        <div className="package-info-section">
+          <div className="package-info-section-div">
+          <i class="fa-brands fa-instagram package-icon"></i>
+            <p className="package-duration">~8 saat</p>
+            <p className="package-name">Sosial şəbəkələr</p>
+          </div>
+          <div className="package-info-section-div">
+            <p className="package-duration">~23 saat</p>
+            <p className="package-name">Mesajlaşma</p>
+          </div>
+          <div className="package-info-section-div">
+            <p className="package-duration">~75 saat</p>
+            <p className="package-name">Musiqi</p>
+          </div>
+
+        </div>
+      <button id="package-button">20❤️</button>
+      </div>
     </div>
   );
 }
