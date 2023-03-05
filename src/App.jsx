a2c8a8b803e45b2c13d16f9b11460615bc472981
Alish Hajiyev <alish.haciyev@mail.ru>
2023-03-05 17:06:30 +0400

    Alish1

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
-import MainPage from './pages/mainpage/MainPage';
-import Buycrypto from './pages/buycrypto/Buycrypto';
-import Charity from './pages/charity/Charity';
-import Azercell from './pages/azercell/Azercell';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import MainPage from "./pages/mainpage/MainPage";
+import Buycrypto from "./pages/buycrypto/Buycrypto";
+import Charity from "./pages/charity/Charity";
+import Azercell from "./pages/azercell/Azercell";
 
-import { Counter } from './features/counter/Counter';
-
-import User from './pages/user/User';
-
+import User from "./pages/user/User";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Counter />
+      <Navbar />
+      
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/buycrypto" element={<Buycrypto />} />
         <Route path="/charity" element={<Charity />} />
         <Route path="/azercell" element={<Azercell />} />
         <Route path="/user" element={<User />} />
-
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
