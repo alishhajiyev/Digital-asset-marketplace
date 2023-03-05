80ad5e685f71b9a9dc68b74a3031e7ec22d15420
Alish Hajiyev <alish.haciyev@mail.ru>
2023-03-05 07:39:12 +0400

    ALish added

---
+++ src/Components/Package/Package.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Package() {
+  return (
+    <div className="packages">
+      <div className="package-background">
+        <h3 className="megabytes">500MB</h3>
+      </div>
+      <div className="package-info"></div>
+    </div>
+  );
+}
