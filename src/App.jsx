890b23ee8af23c01d71b06f0c87ced020c5c1238
turalagakisiyev <agakisiyevtural6@gmail.com>
2023-03-05 04:25:20 -0800

    tural7

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,12 @@ import Buycrypto from './pages/buycrypto/Buycrypto';
 import Charity from './pages/charity/Charity';
 import Azercell from './pages/azercell/Azercell';
 import User from './pages/user/User';
+import Footer from './Components/footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/buycrypto" element={<Buycrypto />} />
@@ -25,6 +26,7 @@ function App() {
         <Route path="/user" element={<User />} />
 
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -6,7 +6,11 @@ import './MainPage.css'
 export default function MainPage() {
   return (
     <div id='MainPage'>
-      <div style={{ display: "flex",padding:"120px" }} className="upside"><h1>Buy, trade, and donate cryptocurrencies on CryptoCell</h1><Link to={"buycrypto"}><button className="MainPage-start"> Get Started </button></Link></div>
+      <div style={{ display: "flex", padding: "120px" }} className="upside">
+        <h1>Buy, trade, and donate cryptocurrencies on CryptoCell</h1>
+        <Link to={"buycrypto"}><button className="MainPage-start"> Get Started </button></Link>
+      </div>
+
       <div className="MainPage-crypto-block">
         <div style={{ color: "orange" }}><i className="fa-brands fa-bitcoin"></i>Bitcoin BTC <br /> $22,329</div>
         <div style={{ color: "blue" }}><i className="fa-brands fa-ethereum"></i>Ethereum ETH <br /> $1.586</div>
