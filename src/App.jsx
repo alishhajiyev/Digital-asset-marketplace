7a09b46390e3df2b687be74774110121d39f0df7
turalagakisiyev <agakisiyevtural6@gmail.com>
2023-03-05 05:23:54 -0800

    salam

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-import React from "react";
-import Navbar from "./Components/Navbar/Navbar";
-
-
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import MainPage from "./pages/mainpage/MainPage";
-import Buycrypto from "./pages/buycrypto/Buycrypto";
-import Charity from "./pages/charity/Charity";
-import Azercell from "./pages/azercell/Azercell";
-
 import {
   BrowserRouter,
   Routes,
@@ -19,18 +9,17 @@ import Buycrypto from './pages/buycrypto/Buycrypto';
 import Charity from './pages/charity/Charity';
 import Azercell from './pages/azercell/Azercell';
 
-import { Counter } from './features/counter/Counter';
-
 import User from './pages/user/User';
 import Footer from './Components/footer/Footer';
+import Navbar from './Components/Navbar/Navbar'
 
 
-import User from "./pages/user/User";
+
 
 function App() {
   return (
     <BrowserRouter>
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/buycrypto" element={<Buycrypto />} />
